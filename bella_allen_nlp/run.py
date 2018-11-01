e70e0ca2f738895c3a234b4ae287c20325b67c5e
Andrew Moore <andrew.p.moore94@gmail.com>
2018-11-01 19:45:40 +0000

    programmable version

---
+++ bella_allen_nlp/run.py
@@ -0,0 +1,112 @@
+from allennlp.common.file_utils import cached_path
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.data.token_indexers import TokenCharactersIndexer, SingleIdTokenIndexer
+from allennlp.modules.token_embedders import Embedding, TokenCharactersEncoder
+from allennlp.modules.token_embedders.embedding import _read_pretrained_embeddings_file
+from allennlp.modules.seq2vec_encoders import CnnEncoder, PytorchSeq2VecWrapper
+from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
+from allennlp.data.iterators import BucketIterator
+from allennlp.training.trainer import Trainer, TensorboardWriter
+
+from tensorboardX import SummaryWriter, FileWriter
+
+import torch
+import torch.optim as optim
+
+from bella_allen_nlp.dataset_readers.target_reader import TargetDatasetReader
+from bella_allen_nlp.allen_models.basic_target_lstm import TargetLSTMClassifier
+
+#token_indexers = {'tokens': SingleIdTokenIndexer(namespace='tokens_id'),
+#                  'chars': TokenCharactersIndexer(namespace='char_id')}
+token_indexers = {'tokens': SingleIdTokenIndexer(namespace='tokens_id', 
+                                                 lowercase_tokens=True)}
+reader = TargetDatasetReader(token_indexers=token_indexers)
+train_dataset = reader.read(cached_path(
+    '/Users/andrew/.Bella/Datasets/SemEval_2014_Resturants_Train'))
+validation_dataset = reader.read(cached_path(
+    '/Users/andrew/.Bella/Datasets/SemEval_2014_Resturants_Dev'))
+target = train_dataset[0].fields['target']
+text = train_dataset[0].fields['text']
+label = train_dataset[0].fields['label']
+
+vocab = Vocabulary.from_instances(train_dataset + validation_dataset)
+WORD_EMBEDDING_DIM = 50
+CHAR_EMBEDDING_DIM = 5
+CHAR_WORD_DIM = 30
+HIDDEN_DIM = 50
+
+
+#char_embedding = Embedding(num_embeddings=vocab.get_vocab_size("char_id"), 
+#                           embedding_dim=CHAR_EMBEDDING_DIM)
+#character_cnn = CnnEncoder(embedding_dim=CHAR_EMBEDDING_DIM, num_filters=2, 
+#                           output_dim=CHAR_WORD_DIM)
+#token_character_encoder = TokenCharactersEncoder(embedding=char_embedding, 
+#                                                 encoder=character_cnn)
+
+#word_embeddings = BasicTextFieldEmbedder({"tokens": token_embedding,
+#                                          "chars": token_character_encoder})
+
+
+# Model
+glove_fp = cached_path('/Users/andrew/Desktop/glove.6B/glove.6B.50d.txt')
+glove_50_weights = _read_pretrained_embeddings_file(glove_fp, 50, vocab, 'tokens_id')
+
+token_embedding = Embedding(num_embeddings=vocab.get_vocab_size('tokens_id'),
+                            embedding_dim=WORD_EMBEDDING_DIM,
+                            weight=glove_50_weights)
+token_embedding1 = Embedding(num_embeddings=vocab.get_vocab_size('tokens_id'),
+                            embedding_dim=WORD_EMBEDDING_DIM)
+id_to_tokens = vocab.get_index_to_token_vocabulary(namespace='tokens_id')
+token_names = list(id_to_tokens.values())
+
+
+word_embeddings = BasicTextFieldEmbedder({"tokens": token_embedding})
+
+text_lstm = PytorchSeq2VecWrapper(torch.nn.LSTM(WORD_EMBEDDING_DIM, HIDDEN_DIM, batch_first=True))
+target_lstm = PytorchSeq2VecWrapper(torch.nn.LSTM(WORD_EMBEDDING_DIM, HIDDEN_DIM, batch_first=True))
+feed_forward = torch.nn.Linear(HIDDEN_DIM * 2,
+                               out_features=vocab.get_vocab_size('labels'))
+
+model = TargetLSTMClassifier(vocab, word_embeddings, text_lstm, target_lstm, feed_forward)
+
+# Data iterator
+sort_fields = [("text", "num_tokens"), ("target", "num_tokens")]
+iterator = BucketIterator(batch_size=32, sorting_keys=sort_fields)
+iterator.index_with(vocab)
+
+# Model training
+optimizer = optim.Adam(model.parameters())
+
+trainer = Trainer(model=model,
+                  optimizer=optimizer,
+                  iterator=iterator,
+                  train_dataset=train_dataset,
+                  validation_dataset=validation_dataset,
+                  patience=10,
+                  num_epochs=40,
+                  histogram_interval=100, should_log_learning_rate=True)
+serialization_dir = '/tmp/anything100'
+import os
+train_log = SummaryWriter(os.path.join(serialization_dir, "log", "train"))
+validation_log = SummaryWriter(os.path.join(serialization_dir, "log", "validation"))
+another_log = SummaryWriter(os.path.join(serialization_dir, "log", "embeddings"))
+another_log.add_embedding(token_embedding.weight, metadata=token_names)
+trainer._tensorboard = TensorboardWriter(train_log=train_log, validation_log=validation_log)
+
+trainer.train()
+
+
+#predictor = SentenceTaggerPredictor(model, dataset_reader=reader)
+#tag_logits = predictor.predict("The dog ate the apple")['tag_logits']
+#tag_ids = np.argmax(tag_logits, axis=-1)
+#print([model.vocab.get_token_from_index(i, 'labels') for i in tag_ids])
+
+# Some type of debugging
+#for c in iterator(train_dataset):
+#    print(c)
+#    print(word_embeddings(c['text']))
+#    word_embeds = word_embeddings(c['text'])
+#     print(word_embeddings(c))
+#    print('something')
+#    break
+#print('anything')
