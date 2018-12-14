035b20ff421515e63300d6b33f6d146226f047f7
Andrew Moore <andrew.p.moore94@gmail.com>
2018-12-14 14:48:39 +0000

    Test the empty right context

--- tests/test_tdlstm_dataset_reader.py
+++ tests/test_tdlstm_dataset_reader.py
@@ -36,14 +36,20 @@ class TestTDLSTMDatasetReader():
                      "target": ["Unda", "(", "Egg", ")", "rolls"],
                      "sentiment": 'positive'}
         # Left text in this case is empty but an empty string is represented as 
-        # the padding token which is Token(text_int=0) which has no text 
-        # representation thus it is equal to None
+        # the @@EMPTY_SENTENCE@@ token. However when the target is included 
+        # this token should not exist
         instance12 = {"left_text": ["@@EMPTY_SENTENCE@@"],
                       "target": ["lava", "cake", "dessert"], 
                       "right_text": ["was", "incredible", "and", "I", 
                                      "recommend", "it", "."],
                       "sentiment": "positive"}
-        test_instances = [instance1, instance5, instance12]
+        # Same as the left context but for the right.
+        instance13 = {"left_text": ["INCREDIBLY", "POOR", "SERVICE", "AN", 
+                                    "FOOD", "QUALITY", "AT", "EXORBITANT"],
+                      "target": ["PRICES"],
+                      "right_text": ["@@EMPTY_SENTENCE@@"],
+                      "sentiment": "negative"}
+        test_instances = [instance1, instance5, instance12, instance13]
         
         if reverse_right_text:
             for test_instance in test_instances:
@@ -53,12 +59,15 @@ class TestTDLSTMDatasetReader():
             for test_instance in test_instances:
                 target = test_instance['target']
                 left_text = test_instance['left_text']
-                # Handle the None case
+                # Handle the empty sentence case
                 if left_text == ["@@EMPTY_SENTENCE@@"]:
                     left_text = []
                 left_text += target
 
                 right_text = test_instance['right_text']
+                if right_text == ["@@EMPTY_SENTENCE@@"]:
+                    right_text = []
+
                 if reverse_right_text:
                     target_temp = copy.deepcopy(target)
                     target_temp.reverse()
@@ -69,7 +78,7 @@ class TestTDLSTMDatasetReader():
                 test_instance['left_text'] = left_text
                 test_instance['right_text'] = right_text
 
-        assert len(instances) == 12
+        assert len(instances) == 13
         fields = instances[0].fields
         assert [t.text for t in fields["left_text"].tokens] == instance1["left_text"]
         assert [t.text for t in fields["right_text"].tokens] == instance1["right_text"]
@@ -85,3 +94,8 @@ class TestTDLSTMDatasetReader():
         assert [t.text for t in fields["right_text"].tokens] == instance12["right_text"]
         assert [t.text for t in fields["target"].tokens] == instance12["target"]
         assert fields["label"].label == instance12["sentiment"]
+        fields = instances[12].fields
+        assert [t.text for t in fields["left_text"].tokens] == instance13["left_text"]
+        assert [t.text for t in fields["right_text"].tokens] == instance13["right_text"]
+        assert [t.text for t in fields["target"].tokens] == instance13["target"]
+        assert fields["label"].label == instance13["sentiment"]
