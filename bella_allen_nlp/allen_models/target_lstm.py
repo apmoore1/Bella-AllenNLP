1c39e46d8078597286227ccb355c29dfc16d7078
Andrew Moore <andrew.p.moore94@gmail.com>
2018-12-14 12:11:10 +0000

    LSTM based models

--- bella_allen_nlp/allen_models/target_lstm.py
+++ bella_allen_nlp/allen_models/target_lstm.py
@@ -1,18 +1,15 @@
 from typing import Dict, Optional
 
-import numpy
-from overrides import overrides
-import torch
-import torch.nn.functional as F
-
-from allennlp.common import Params
 from allennlp.common.checks import ConfigurationError
 from allennlp.data import Vocabulary
 from allennlp.modules import FeedForward, Seq2VecEncoder, TextFieldEmbedder
 from allennlp.models.model import Model
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
 from allennlp.nn import util
-from allennlp.training.metrics import CategoricalAccuracy
+from allennlp.training.metrics import CategoricalAccuracy, F1Measure
+import numpy as np
+import torch
+import torch.nn.functional as F
 
 '''
 Seq2VecEncoder is an abstract method that maps tensors of shape 
@@ -27,8 +24,8 @@ class TargetLSTMClassifier(Model):
                  vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
                  text_encoder: Seq2VecEncoder,
-                 target_encoder: Seq2VecEncoder,
                  classifier_feedforward: FeedForward,
+                 target_encoder: Optional[Seq2VecEncoder] = None,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super().__init__(vocab, regularizer)
@@ -39,9 +36,15 @@ class TargetLSTMClassifier(Model):
         self.target_encoder = target_encoder
         self.classifier_feedforward = classifier_feedforward
         self.metrics = {
-                "accuracy": CategoricalAccuracy()#,
-                #"accuracy3": CategoricalAccuracy(top_k=3)
+                "accuracy": CategoricalAccuracy()
         }
+        self.f1_metrics = {}
+        # F1 Scores
+        label_index_name = self.vocab.get_index_to_token_vocabulary('labels')
+        for label_index, label_name in label_index_name.items():
+            label_name = f'F1_{label_name.capitalize()}'
+            self.f1_metrics[label_name] = F1Measure(label_index)
+    
         self.loss = torch.nn.CrossEntropyLoss()
         initializer(self)
 
@@ -59,22 +62,22 @@ class TargetLSTMClassifier(Model):
         text_mask = util.get_text_field_mask(text)
         encoded_text = self.text_encoder(embedded_text, text_mask)
 
-        embedded_target = self.text_field_embedder(target)
-        target_mask = util.get_text_field_mask(target)
-        encoded_target = self.target_encoder(embedded_target, target_mask)
+        if self.target_encoder:
+            embedded_target = self.text_field_embedder(target)
+            target_mask = util.get_text_field_mask(target)
+            encoded_target = self.target_encoder(embedded_target, target_mask)
+            encoded_text = torch.cat([encoded_text, encoded_target], dim=-1)
 
-        encoded_text_target = torch.cat([encoded_text, encoded_target], dim=-1)
-        logits = self.classifier_feedforward(encoded_text_target)
+        logits = self.classifier_feedforward(encoded_text)
         class_probabilities = F.softmax(logits, dim=-1)
 
         output_dict = {"class_probabilities": class_probabilities}
 
         if label is not None:
             loss = self.loss(logits, label)
-            #loss = self.loss(logits, label.squeeze(-1))
-            for metric in self.metrics.values():
-                metric(logits, label)
-                #metric(logits, label.squeeze(-1))
+            for metrics in [self.metrics, self.f1_metrics]:
+                for metric in metrics.values():
+                    metric(logits, label)
             output_dict["loss"] = loss
 
         return output_dict
@@ -82,12 +85,22 @@ class TargetLSTMClassifier(Model):
     def decode(self, output_dict: Dict[str, torch.Tensor]
                ) -> Dict[str, torch.Tensor]:
         predictions = output_dict['class_probabilities'].cpu().data.numpy()
-        argmax_indices = numpy.argmax(predictions, axis=-1)
+        argmax_indices = np.argmax(predictions, axis=-1)
         labels = [self.vocab.get_token_from_index(x, namespace="labels")
                   for x in argmax_indices]
         output_dict['label'] = labels
         return output_dict
 
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-        return {metric_name: metric.get_metric(reset) 
-                for metric_name, metric in self.metrics.items()}
+        # Other scores
+        metric_name_value = {}
+        for metric_name, metric in self.metrics.items():
+            metric_name_value[metric_name] = metric.get_metric(reset)
+        # F1 scores
+        all_f1_scores = []
+        for metric_name, metric in self.f1_metrics.items():
+            precision, recall, f1_measure = metric.get_metric(reset)
+            all_f1_scores.append(f1_measure)
+            metric_name_value[metric_name] = f1_measure
+        metric_name_value['Macro_F1'] = sum(all_f1_scores) / len(self.f1_metrics)
+        return metric_name_value
