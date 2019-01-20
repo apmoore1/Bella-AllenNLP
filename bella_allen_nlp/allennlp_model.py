c3fa90017a6a950b2d19b6c382529dfdbd12c837
Andrew Moore <andrew.p.moore94@gmail.com>
2019-01-20 22:44:56 +0000

    API almost complete

--- bella_allen_nlp/allennlp_model.py
+++ bella_allen_nlp/allennlp_model.py
@@ -1,30 +1,105 @@
-from typing import Optional
+from typing import Optional, List
 import json
 import tempfile
 from pathlib import Path
+import random
 
+from allennlp.common.params import Params
+from allennlp.commands.train import train_model_from_file
+from allennlp.data.dataset_readers import DatasetReader
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.models import Model
+from allennlp.models.archival import load_archive
 from bella.data_types import TargetCollection
 
 class AllenNLPModel():
     '''
     This creates a wrapper for all of the Allen NLP models that will be 
-    compatible closely comptabile with the bella.models.base.BaseModel 
-    interface
+    compatible closely with the bella.models.base.BaseModel interface
+
+    To predict I might have to implement a predictor.
     '''
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, model_param_fp: Path, 
+                 save_dir: Optional[Path] = None) -> None:
         '''
         :param name: Name of the model e.g. TDLSTM or IAN
+        :param model_params_fp: File path to the model parameters that will 
+                                define the AllenNLP model and how to train it.
+        :param save_dir: Directory to save the model to. This has to be set
+                         up front as the fit function saves the model each 
+                         epoch.
         '''
 
         self.name = name
+        self.model = None
+        self.save_dir = save_dir
         self._fitted = False
+        self._param_fp = model_param_fp
 
     def fit(self, train_data: TargetCollection, val_data: TargetCollection,
-            test_data: Optional[TargetCollection] = None
-            ) -> None:
-        train_data.to_json_file()
+            test_data: Optional[TargetCollection] = None) -> None:
+        '''
+        Given the training, validation, and optionally the test data it will 
+        train the model that is defined in the model params file provided as 
+        argument to the constructor of the class. Once trained the model can 
+        be accessed through the `model` attribute.
 
+        NOTE: If the test data is given the model only uses it to fit to the 
+        vocabularly that is within the test data, the model NEVER trains on 
+        the test data.
+
+        :param train_data: Training data.
+        :param val_data: Validation data.
+        :param test_data: Optional, test data.
+        '''
+
+        model_params = self._preprocess_and_load_param_file(self._param_fp)
+        # Ensures that a different random seed is used each time
+        self._set_random_seeds(model_params)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            train_fp = Path(temp_dir, 'train_data.json')
+            val_fp = Path(temp_dir, 'val_data.json')
+
+            self._data_to_json(train_data, train_fp)
+            self._data_to_json(val_data, val_fp)
+            if test_data:
+                test_fp = Path(temp_dir, 'test_data.json')
+                self._data_to_json(test_data, test_fp)
+                self._add_dataset_paths(model_params, train_fp, val_fp, test_fp)
+                model_params["evaluate_on_test"] = True
+            else:
+                self._add_dataset_paths(model_params, train_fp, val_fp)
+
+            save_dir = self.save_dir if self.save_dir else Path(temp_dir, 
+                                                                'temp_save_dir')
+            
+            temp_param_fp = Path(temp_dir, 'temp_param_file.json')
+            model_params.to_file(temp_param_fp.resolve())
+            trained_model = train_model_from_file(temp_param_fp, save_dir)
+            self.model = trained_model
+        self.fitted = True
+
+    def load(self, cuda_device: int = -1) -> Model:
+        '''
+        Loads the model. This does not require you to train the model if the 
+        `save_dir` attribute is pointing to a folder containing a trained model.
+
+        This is just a wrapper around the `load_archive` function.
+
+        :param cuda_device: Whether the loaded model should be loaded on to the 
+                            CPU (-1) or the GPU (0). Default CPU.
+        :returns: The model that was saved at `self.save_dir` 
+        '''
+
+        save_dir_err = 'Save directory was not set in the constructor of the class'
+        assert self.save_dir, save_dir_err
+        if self.save_dir.exists():
+            return load_archive(self.save_dir / "model.tar.gz", 
+                                cuda_device=cuda_device)
+        raise FileNotFoundError('There is nothing at the save dir:\n'
+                                f'{self.save_dir.resolve()}')
+        
     @staticmethod
     def _data_to_json(data: TargetCollection, file_path: Path) -> None:
         '''
@@ -42,7 +117,71 @@ class AllenNLPModel():
                     json_encoded_data = f'\n{json_encoded_data}'
                 json_file.write(json_encoded_data)
 
-        
+    @staticmethod
+    def _preprocess_and_load_param_file(model_param_fp: Path) -> Params:
+        '''
+        Given a model parameter file it will load it as a Params object and 
+        remove all data fields for the Param object so that these keys can be 
+        added with different values associated to them.
+
+        fields (keys) that are removed:
+        1. train_data_path
+        2. validation_data_path
+        3. test_data_path
+        4. evaluate_on_test
+
+        :param model_param_fp: File path to the model parameters that will 
+                               define the AllenNLP model and how to train it.
+        :returns: The model parameter file as a Params object with the data 
+                   fields removed if they exisited.
+        '''
+
+        model_param_fp = str(model_param_fp.resolve())
+        fields_to_remove = ['train_data_path', 'validation_data_path', 
+                            'test_data_path', 'evaluate_on_test']
+        model_params = Params.from_file(model_param_fp)
+        for field in fields_to_remove:
+            if field in model_params:
+                model_params.pop(field)
+        return model_params
+
+    @staticmethod
+    def _add_dataset_paths(model_params: Params, train_fp: Path, val_fp: Path, 
+                           test_fp: Optional[Path] = None) -> None:
+        '''
+        Give model parameters it will add the given train, validation and 
+        optional test dataset paths to the model parameters.
+
+        Does not return anything as the model parameters object is mutable
+
+        :param model_params: model parameters to add the dataset paths to
+        :param train_fp: Path to the training dataset
+        :param val_fp: Path to the validation dataset
+        :param test_fp: Optional path to the test dataset
+        '''
+
+        model_params['train_data_path'] = str(train_fp.resolve())
+        model_params['validation_data_path'] = str(val_fp.resolve())
+        if test_fp:
+            model_params['test_data_path'] = str(test_fp.resolve())
+
+    @staticmethod
+    def _set_random_seeds(model_params: Params) -> None:
+        '''
+        This ensures to some extent that the experiments are NOT reproducible 
+        so that we can take into account the random seed problem.
+
+        Returns nothing as the model_params will be modified as they are a 
+        mutable object.
+
+        :param model_params: The parameters of the model
+        '''
+
+        seed, numpy_seed, torch_seed = [random.randint(1,99999) 
+                                        for i in range(3)]
+        model_params["random_seed"] = seed
+        model_params["numpy_seed"] = numpy_seed
+        model_params["pytorch_seed"] = torch_seed     
 
     def __repr__(self) -> str:
         '''
@@ -69,3 +208,23 @@ class AllenNLPModel():
         '''
 
         self._fitted = value
+
+    # Not required anymore
+    @staticmethod
+    def _get_vocab(dataset_reader: DatasetReader, data_paths: List[Path]
+                   ) -> Vocabulary:
+        '''
+        It will create a vocabulary object from the dataset reader given and 
+        the data files provided in the data_paths list.
+
+        :param dataset_reader: A dataset reader than can read the data provided 
+                               in the data paths list.
+        :param data_paths: A list of paths to the data to be read e.g. a list 
+                           that contains the training data file path, validation 
+                           data file path, and the test data file path.
+        :returns: A vocabulary object from the data and dataset reader.
+        '''
+        all_instances = []
+        for data_path in data_paths:
+            all_instances.extend(list(dataset_reader.read(data_path)))
+        return Vocabulary.from_instances(all_instances)
--- tests/allennlp_model_test.py
+++ tests/allennlp_model_test.py
@@ -2,9 +2,11 @@ import tempfile
 from pathlib import Path
 
 from allennlp.common.util import ensure_list
+from allennlp.common.params import Params
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from bella.parsers import semeval_14
 import pytest
+from flaky import flaky
 
 from bella_allen_nlp import AllenNLPModel
 from bella_allen_nlp.dataset_readers.target import TargetDatasetReader
@@ -14,35 +16,176 @@ class TestAllenNLPModel():
 
     
     test_dir = Path(__file__, '..', 'test_data')
+    
+    train_data_fp = Path(test_dir, 'data', 'target_collection_train_data.xml')
     test_data_fp = Path(test_dir, 'data', 'target_collection_test_data.xml')
+    
     TARGET_DATA = semeval_14(test_data_fp, name='test data')
+    TARGET_TRAIN_DATA = semeval_14(train_data_fp, name='train data')
+    
+    test_model_dir = Path(test_dir, 'model_configs')
+    MODEL_TARGET_FP = Path(test_model_dir, 'test_target_model_config.json')
+    MODEL_TDLSTM_FP = Path(test_model_dir, 'test_tdlstm_model_config.json')
 
     def test_repr_test(self):
-        model = AllenNLPModel('ML')
+        model = AllenNLPModel('ML', self.MODEL_TARGET_FP)
         model_repr = model.__repr__()
         assert model_repr == 'ML'
 
     def test_fitted(self):
-        model = AllenNLPModel('ML')
+        model = AllenNLPModel('ML', self.MODEL_TARGET_FP)
         assert not model.fitted
 
         model.fitted = True
         assert model.fitted
 
+    @pytest.mark.parametrize("test_data", (True, False))
+    def test_fit(self, test_data):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_save_dir_fp = Path(temp_dir, 'test save dir')
+            model = AllenNLPModel('ML', self.MODEL_TARGET_FP, temp_save_dir_fp)
+            assert not model.fitted
+            if test_data:
+                model.fit(self.TARGET_TRAIN_DATA, self.TARGET_TRAIN_DATA, 
+                          self.TARGET_DATA)
+            else:
+                model.fit(self.TARGET_TRAIN_DATA, self.TARGET_TRAIN_DATA)
+            assert model.fitted
+            assert temp_save_dir_fp.is_dir()
+            token_index = model.model.vocab.get_token_to_index_vocabulary('tokens')
+            if test_data:
+                assert 'Tais' in list(token_index.keys())
+            else:
+                assert 'Tais' not in list(token_index.keys())
+
+    @pytest.mark.parametrize("test_data", (True, False))
+    def test_load(self, test_data):
+        # Testing that an error is raised when there is no save directory
+        model = AllenNLPModel('ML', self.MODEL_TARGET_FP)
+        with pytest.raises(Exception):
+            model.load()
+        model.fit(self.TARGET_TRAIN_DATA, self.TARGET_TRAIN_DATA)
+        assert model.fitted
+        with pytest.raises(Exception):
+            model.load()
+        # Testing when the save directory is given
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_save_dir_fp = Path(temp_dir, 'test save dir')
+            model = AllenNLPModel('ML', self.MODEL_TARGET_FP, temp_save_dir_fp)
+            with pytest.raises(FileNotFoundError):
+                model.load()
+            with pytest.raises(FileNotFoundError):
+                temp_save_dir_fp.mkdir()
+                model.load()
+            temp_save_dir_fp.rmdir()
+            if test_data:
+                model.fit(self.TARGET_TRAIN_DATA, self.TARGET_TRAIN_DATA, 
+                          self.TARGET_DATA)
+            else:
+                model.fit(self.TARGET_TRAIN_DATA, self.TARGET_TRAIN_DATA)
+            archived_model = model.load()
+            archived_model = archived_model.model
+            token_index = archived_model.vocab.get_token_to_index_vocabulary('tokens')
+            assert len(token_index) > 10
+            if test_data:
+                assert 'Tais' in list(token_index.keys())
+            else:
+                assert 'Tais' not in list(token_index.keys())
+
+        
+
+    @flaky
+    def test_set_random_seeds(self):
+        model_params = Params.from_file(self.MODEL_TARGET_FP.resolve())
+        seed_keys = ["random_seed", "numpy_seed", "pytorch_seed"]
+        for key in seed_keys:
+            assert key not in model_params
+        AllenNLPModel._set_random_seeds(model_params)
+        seed_values = {}
+        for key in seed_keys:
+            assert key in model_params
+            seed_values[key] = model_params[key]
+        AllenNLPModel._set_random_seeds(model_params)
+        for key in seed_keys:
+            assert seed_values[key] != model_params[key]
+        
+
+    @pytest.mark.parametrize("lazy", (True, False))
+    def test_get_vocab(self, lazy):
+        def tokens_labels_exist(dataset_reader, data_paths, test=False):
+            true_labels = sorted(['positive', 'negative', 'neutral'])
+
+            vocab = AllenNLPModel._get_vocab(dataset_reader, data_paths)
+            tokens = list(vocab.get_token_to_index_vocabulary('tokens').keys())
+            labels = list(vocab.get_token_to_index_vocabulary('labels').keys())
+            labels = sorted(labels)
+            assert true_labels == labels
+
+            assert len(tokens) > 0
+            if test:
+                assert 'Tais' in tokens
+                assert 'Tha' in tokens
+            else:
+                assert 'Tais' not in tokens
+                assert 'Tha' not in tokens 
+
+        with tempfile.TemporaryDirectory() as temp_data_dir:
+            train_fp = Path(temp_data_dir, 'train data')
+            AllenNLPModel._data_to_json(self.TARGET_TRAIN_DATA, train_fp)
+            val_fp = Path(temp_data_dir, 'val data')
+            AllenNLPModel._data_to_json(self.TARGET_TRAIN_DATA, val_fp)
+            test_fp = Path(temp_data_dir, 'test data')
+            AllenNLPModel._data_to_json(self.TARGET_DATA, test_fp)
+                
+            dataset_reader = TargetDatasetReader(lazy=lazy)
+            tokens_labels_exist(dataset_reader, [train_fp, val_fp], test=False)
+            tokens_labels_exist(dataset_reader, [train_fp, val_fp, test_fp], 
+                                test=True)
+            
+
+    def test_preprocess_and_load_param_file(self):
+        fields_to_remove = ['train_data_path', 'validation_data_path', 
+                            'test_data_path', 'evaluate_on_test']
+        
+        model_params = AllenNLPModel._preprocess_and_load_param_file(self.MODEL_TARGET_FP)
+        for field in fields_to_remove:
+            assert field not in model_params
+
+        model_params = Params.from_file(self.MODEL_TARGET_FP.resolve())
+        for field in fields_to_remove:
+            assert field in model_params
+
+    def test_add_dataset_paths(self):
+        model_params = AllenNLPModel._preprocess_and_load_param_file(self.MODEL_TARGET_FP)
+        train_path = Path(self.test_data_fp, 'train_data')
+        val_path = Path(self.test_data_fp, 'val_data')
+        AllenNLPModel._add_dataset_paths(model_params, train_path, val_path)
+        assert str(train_path.resolve()) == model_params['train_data_path']
+        assert str(val_path.resolve()) == model_params['validation_data_path']
+        with pytest.raises(KeyError):
+            model_params['test_data_path']
+        
+        model_params = AllenNLPModel._preprocess_and_load_param_file(self.MODEL_TARGET_FP)
+        test_path = Path(self.test_data_fp, 'test_data')
+        AllenNLPModel._add_dataset_paths(model_params, train_path, val_path, test_path)
+        assert str(train_path.resolve()) == model_params['train_data_path']
+        assert str(val_path.resolve()) == model_params['validation_data_path']
+        assert str(test_path.resolve()) == model_params['test_data_path']
+
     @pytest.mark.parametrize("dataset_reader", [TargetDatasetReader(),
                                                 TDLSTMDatasetReader()])
     def test_data_to_json(self, dataset_reader: DatasetReader):
-        model = AllenNLPModel('ML')
+        model = AllenNLPModel('ML', self.MODEL_TARGET_FP)
         with tempfile.TemporaryDirectory() as temp_dir:
             temp_file = Path(temp_dir, 'temp_file.json')
             model._data_to_json(self.TARGET_DATA, temp_file)
             # Need to now read it with multiple dataset readers as in TDLSTM and target
             reader = dataset_reader
             instances = ensure_list(reader.read((temp_file)))
-            text_1 = ['The', 'phone', 'came', 'with', 'a', 'very', 'good', 
+            text_1 = ['Tha', 'phone', 'came', 'with', 'a', 'very', 'good', 
                       'battery', 'life', ',', 'however', 'the', 'phone', 'was', 
                       'not', 'very', 'good', '.']
-            text_2 = ['This', 'was', 'an', 'ok', 'camera', 'but', 'the', 
+            text_2 = ['Tais', 'was', 'an', 'ok', 'camera', 'but', 'the', 
                       'lens', 'could', 'have', 'been', 'better', '.']
             if isinstance(dataset_reader, TargetDatasetReader):
                 test_instances = [{"text": text_1, "target": ["battery", "life"],
