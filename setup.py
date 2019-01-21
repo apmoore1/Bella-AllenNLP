2e4bde2a2c86f4d9d3747e9d5a05c377237066f6
Andrew Moore <andrew.p.moore94@gmail.com>
2019-01-21 18:51:18 +0000

    PyPi setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(name='bella_allen_nlp',
+      version='0.1.0',
+      description='Target Dependent Sentiment Analysis (TDSA) Models written in AllenNLP',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/apmoore1/Bella-AllenNLP',
+      author='Andrew Moore',
+      author_email='andrew.p.moore94@gmail.com',
+      license='MIT',
+      install_requires=[
+          'bella-tdsa==0.2.6',
+          'allennlp==0.8.1',
+          'pytest>4.1.0',
+          'pylint'
+      ],
+      python_requires='>=3.6.1',
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Text Processing',
+          'Topic :: Text Processing :: Linguistic',
+      ])
