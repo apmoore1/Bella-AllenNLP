278387ff4800f6a968165f7ec99b860b12ee335f
Andrew Moore <andrew.p.moore94@gmail.com>
2019-02-08 00:34:42 +0000

    Update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='bella_allen_nlp',
-      version='0.1.43',
+      version='0.1.44',
       description='Target Dependent Sentiment Analysis (TDSA) Models written in AllenNLP',
       long_description=long_description,
       long_description_content_type="text/markdown",
